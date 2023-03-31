604db65bed68297dd393862f3b66d133a23adfaf
Yann Attakoui <yannatkk@gmail.com>
2023-03-31 17:14:01 +0200

    refactor: remove player from the screen

--- lib/features/home/presentation/screens/home_view.dart
+++ lib/features/home/presentation/screens/home_view.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:stopify/constants/constants.dart';
+import 'package:stopify/constants/app_colors.dart';
 import 'package:stopify/features/home/data/datasources/dump_data.dart';
 import 'package:stopify/features/home/presentation/state/view_manager.dart';
-import 'package:stopify/features/home/presentation/widgets/custom_player.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -29,32 +28,7 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const PlayList(),
-            const Spacer(),
-            CustomPlayer(viewManager: _viewManager),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class PlayList extends StatelessWidget {
-  const PlayList({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: Constants.playlistBoxHeight,
-      width: MediaQuery.of(context).size.width,
-      child: ListView.separated(
+      body: ListView.separated(
         itemCount: playList.length,
         itemBuilder: (context, index) {
           return ListTile(
@@ -64,7 +38,12 @@ class PlayList extends StatelessWidget {
               style: const TextStyle(fontWeight: FontWeight.w700),
             ),
             subtitle: Text(playList[index].artist),
-            trailing: const Icon(Icons.play_arrow),
+            trailing: IconButton(
+                icon: const Icon(
+                  Icons.download_for_offline,
+                  color: AppColors.primaryColor,
+                ),
+                onPressed: () {}),
           );
         },
         separatorBuilder: (context, index) => const Divider(thickness: 3),
