9513d899cb2322947524677acd09cd96384f929d
Yann Attakoui <yannatkk@gmail.com>
2023-04-28 03:40:03 +0200

    refactor: share PlayerProgressBar

--- lib/features/listen/presentation/widgets/custom_player/listen_player_progress_bar.dart
+++ lib/features/listen/presentation/widgets/custom_player/listen_player_progress_bar.dart
@@ -1,10 +1,10 @@
 import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:stopify/features/listen/presentation/notifiers/progress_notifier.dart';
 import 'package:stopify/features/listen/presentation/state/playlist_manager.dart';
+import 'package:stopify/shared/utils/progress_notifier.dart';
 
-class PlayerProgressBar extends StatelessWidget {
-  const PlayerProgressBar({
+class ListenPlayerProgressBar extends StatelessWidget {
+  const ListenPlayerProgressBar({
     super.key,
     required PlaylistManager playlistManager,
   }) : _playlistManager = playlistManager;
