b19607b296ad1b58d9a41949dca312fd4b9bce52
Yann Attakoui <yannatkk@gmail.com>
2023-04-29 11:14:02 +0200

    refactor: initialize flutter_downloader

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:stopify/constants/constants.dart';
 import 'package:stopify/features/radio/presentation/screens/radio_player.dart';
 import 'package:stopify/routing/app_router.dart';
 import 'package:stopify/shared/presentation/screens/app_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Plugin must be initialized before using
+  await FlutterDownloader.initialize(
+      debug:
+          true, // optional: set to false to disable printing logs to console (default: true)
+      ignoreSsl:
+          true // option: set to false to disable working with http links (default: false)
+      );
+
   runApp(const MyApp());
 }
 
