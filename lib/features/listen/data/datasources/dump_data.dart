42fb9edc77ac2e34fb8cb7d7134ec1ea1d9e5163
Yann Attakoui <yannatkk@gmail.com>
2023-04-30 12:22:31 +0200

    refactor: replace random id by song url

--- lib/features/listen/data/datasources/dump_data.dart
+++ lib/features/listen/data/datasources/dump_data.dart
@@ -1,7 +1,6 @@
 import 'package:just_audio/just_audio.dart';
 import 'package:just_audio_background/just_audio_background.dart';
 import 'package:tuple/tuple.dart';
-import 'package:uuid/uuid.dart';
 
 const prefix = 'https://www.soundhelix.com/examples/mp3';
 
@@ -16,19 +15,13 @@ List<Tuple2<String, String>> generateSongsList() {
 
 final songsList = generateSongsList();
 
-var id = const Uuid().v4();
-
 final playlist = ConcatenatingAudioSource(
   children: [
     for (var song in songsList)
       AudioSource.uri(
         Uri.parse(song.item1),
-        // tag: {
-        //   'item1': song.item1,
-        //   'item2': song.item2,
-        // },
         tag: MediaItem(
-          id: id,
+          id: song.item1,
           title: song.item2,
           album: song.item1,
         ),
